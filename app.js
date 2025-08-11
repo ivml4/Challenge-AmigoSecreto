e89c52ec547ae444d141a8be82505110cb781424
Ivan Maldonado <ivanleandromaldonado4@gmail.com>
2025-08-11 15:31:28 -0300

    Primer Commit

---
+++ app.js
@@ -0,0 +1,59 @@
+//array que almacena los nombres de los amigos
+let amigos = [];
+
+//funcion para agregar un amigo
+function agregarAmigo() {
+  const inputAmigo = document.getElementById('amigo');
+  const nombreAmigo = inputAmigo.value.trim();
+
+  //limpiar campo de entrada
+  inputAmigo.value = "";
+
+  //valida que el campo no este vacio
+  if (nombreAmigo === "") {
+    alert("Por favor, inserte un nombre.");
+    return; //detiene la ejecucion de la funcion
+  }
+
+  //validar que el nombre no este duplicado
+  if (amigos.includes(nombreAmigo)) {
+    alert(`El nombre ${nombreAmigo} ya esta en la lista`);
+    return;
+  }  
+
+  //agregar el nombre a la array de amigos
+  amigos.push(nombreAmigo);
+  console.log(amigos)
+
+
+  //actualizar la lista en el HTML
+  actualizarLista();
+
+}
+
+function actualizarLista(){
+  const listaAmigos = document.getElementById('listaAmigos');
+  listaAmigos.innerHTML = "";
+  for (let index = 0; index < amigos.length; index++) {
+    const nombreAmigo = amigos[index];
+    let amigoLi = document.createElement('li');
+    
+    amigoLi.textContent= nombreAmigo;
+
+    listaAmigos.appendChild(amigoLi);
+    
+  }
+}
+
+function sortearAmigo(){
+    if (amigos.length === 0) {
+        document.getElementById('listaAmigos').innerHTML = 'agrega un nombre.';
+        return;
+    }
+
+    let index = Math.floor(Math.random() * amigos.length);
+    let amigoSorteado = amigos[index];
+
+    document.getElementById('resultado').innerHTML = `El amigo sorteado es: ${amigoSorteado}`;
+
+}
